1c997c80c853bc5a28535e5ea6bdcb96ba1b5320
vallove89 <vallove89@outlook.com>
2024-12-26 15:06:40 -0600

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,11 +30,11 @@ export default function App() {
   }
   
     
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
